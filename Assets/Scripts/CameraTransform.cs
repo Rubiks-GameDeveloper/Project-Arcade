5af8f6e58320c1e4b7eea7af6107ff7fc8a8bfaf
Rubiks-GameDeveloper <85067996+Rubiks-GameDeveloper@users.noreply.github.com>
2021-10-05 18:03:25 +0300

    Сделал максимально кривое передвижение и камеру, просто чтобы было(скрипт камеры не дописан от слова СОВСЕМ)

---
+++ Assets/Scripts/CameraTransform.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraTransform : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    public static bool isPlayerMove = false;
+
+    private void Start()
+    {
+        GetComponent<Camera>().orthographicSize = 5;
+    }
+
+    private void FixedUpdate()
+    {
+        if (player.transform.position.y > -2f && isPlayerMove)
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 0.3f, -10);
+        }
+    }
+}
---
+++ Assets/Scripts/Prototype_PlayerMovementRight.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Prototype_PlayerMovementRight : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    //Весь код ублютский, сразу говорю, перемещениее персонажа не должно быть в кнопке(привязано) )))))
+
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speedRight;
+
+    private bool isTapInButton = false;
+    //Проверяю, нажал ли игрок в область кнопки(правой)
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        CameraTransform.isPlayerMove = true;
+        isTapInButton = true;
+    }
+    //Проверяю, убрал ли игрок палец с кнопки(правой)
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        CameraTransform.isPlayerMove = false;
+        isTapInButton = false;
+    }
+    private void FixedUpdate()
+    {
+        //Перемещение вправо
+        if (isTapInButton) player.transform.position += new Vector3(speedRight * Time.deltaTime, 0, 0);
+    }
+}
