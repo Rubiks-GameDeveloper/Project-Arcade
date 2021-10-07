e628cbc60470f3be16a5283be9eb21fed723c428
Rubiks-GameDeveloper <85067996+Rubiks-GameDeveloper@users.noreply.github.com>
2021-10-07 22:59:55 +0300

    Слил две ветки и получилась эль проблемо

---
+++ Assets/Scripts/Prototype_Jump.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prototype_Jump : MonoBehaviour
+{
+    //Вообще почти всё становится понятно из переменных, поэтому дерзай)
+    [SerializeField] private AnimationCurve _YAnimation;
+
+    [SerializeField] private float _duration = 1;
+    [SerializeField] private float _height = 1;
+    public void PlayAnimation(Transform jumper, float duration)
+    {
+        //Запускаем корутину (советую прочитать об этом официальную документацию, очень полезная вещь)
+        StartCoroutine(AnimationPlaying(jumper, duration));
+    }
+
+    private IEnumerator AnimationPlaying(Transform jumper, float duration)
+    {
+        float expiriedTime = 0;
+        float progress = 0;
+
+        Vector3 startPosition = jumper.position; 
+
+        while (progress < 1)
+        {
+            expiriedTime += Time.deltaTime;
+
+            //Изменение прогресса прыжка
+            progress = expiriedTime / duration;
+
+            //Изменяем позицию (прибавляем) по оси Y
+            jumper.position += new Vector3(0, _YAnimation.Evaluate(progress) * _height, 0);
+
+            yield return null;
+        }
+    }
+    private void Update()
+    {
+        //Проверяем нажатие кнопки
+        if (Input.GetButtonUp("Jump"))
+        {
+            //Если нажата, то выполняем метод
+            PlayAnimation(transform, _duration);
+        }
+    }
+}
