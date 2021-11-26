1d0f73b44b19ed127e124b9c95703b956f64a239
Rubiks-GameDeveloper <85067996+Rubiks-GameDeveloper@users.noreply.github.com>
2021-11-26 23:20:41 +0300

    Почистил код
    Убрал лишние строчки из PrototypeJump.

--- Assets/Scripts/PrototypeJump.cs
+++ Assets/Scripts/PrototypeJump.cs
@@ -1,49 +1,48 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PrototypeJump : MonoBehaviour
 {
     [SerializeField] private AnimationCurve _YAnimation;
 
-    [SerializeField] private float _duration = 1;
-    [SerializeField] private float _height = 1;
+    [SerializeField] private float duration = 1;
+    [SerializeField] private float height = 1;
 
     [SerializeField] private Transform groundChecker;
 
     private bool _onGround;
     private bool _jumped;
 
+    private Coroutine _currentJump;
+    
     private int _jumpCount = 2;
-    public void PlayAnimation(Transform jumper, float duration)
+    private void PlayAnimation(Transform jumper, float duration)
     {
         if (_onGround)
         {
             _jumpCount -= 1;
-            StartCoroutine(AnimationPlaying(jumper, duration));
+            _currentJump = StartCoroutine(AnimationPlaying(jumper, duration));
         }
         else if (_jumped && _jumpCount > 0)
         {
             _jumpCount -= 1;
+            StopCoroutine(_currentJump);
             StartCoroutine(AnimationPlaying(jumper, duration));
         }
     }
 
-    private IEnumerator AnimationPlaying(Transform jumper, float duration)
+    private IEnumerator AnimationPlaying(Transform jumper, float jumpDuration)
     {
         float expiredTime = 0;
         float progress = 0;
-
-        Vector3 startPosition = jumper.position; 
 
         while (progress < 1)
         {
             expiredTime += Time.fixedDeltaTime * 0.1f;
             
-            progress = expiredTime / duration;
+            progress = expiredTime / jumpDuration;
             
-            jumper.position += new Vector3(0, _YAnimation.Evaluate(progress) * _height, 0);
+            jumper.position += new Vector3(0, _YAnimation.Evaluate(progress) * height, 0);
 
             yield return null;
         }
@@ -52,19 +51,18 @@ public class PrototypeJump : MonoBehaviour
     private void FixedUpdate()
     {
         GroundCheck();
-        //if (_onGround && !_jumped) _jumpCount = 2;
     }
 
     private void GroundCheck()
     {
-        Collider2D[] _colliders = new Collider2D[25];
-        var res = Physics2D.OverlapCircleNonAlloc(groundChecker.position, 0.6f, _colliders);
+        Collider2D[] colliders = new Collider2D[25];
+        var res = Physics2D.OverlapCircleNonAlloc(groundChecker.position, 0.6f, colliders);
  
         if (res > 1)
         {
-            foreach (var item in _colliders)
+            foreach (var item in colliders)
             {
-                if (item != null && item.gameObject.CompareTag("Ground"))
+                if (item.gameObject.CompareTag("Ground"))
                 {
                     _onGround = true;
                     _jumped = false;
@@ -79,6 +77,6 @@ public class PrototypeJump : MonoBehaviour
     public void PlayerJump()
     {
         if (_onGround && !_jumped) _jumpCount = 2;
-        PlayAnimation(transform, _duration);
+        PlayAnimation(transform, duration);
     }
 }
